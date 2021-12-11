6b8ca5743489dd6d0472245b4f20edc456fe4ce7
Andresf <andresr427@gmail.com>
2021-12-11 10:44:17 -0500

    cambios en el diseño login y sign up

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="signUp_user">
         <div class="container_signUp_user">
-            <h2>Create an Account</h2>
+            <h2>Crear una Cuenta</h2>
             <form v-on:submit.prevent="processSignUp">
-                <input type="text" v-model="user.username" placeholder="Username" />
+                <input type="text" v-model="user.username" placeholder="Usuario" />
                 <br />
-                <input type="password" v-model="user.password" placeholder="Password" />
+                <input type="password" v-model="user.password" placeholder="Contraseña" />
                 <br />
-                <input type="text" v-model="user.name" placeholder="Name" />
+                <input type="text" v-model="user.name" placeholder="Nombre" />
                 <br />
                 <input type="email" v-model="user.email" placeholder="Email" />
-                <button type="submit">Submit</button>
+                <button type="submit">Registrarse</button>
             </form>
         </div>
     </div>
@@ -71,7 +71,7 @@ export default {
     align-items: center;
 }
 .container_signUp_user {
-    border: solid #a26464;
+    border: solid #5c1c1c;
     border-radius: 10px;
     width: 35%;
     height: 80%;
@@ -96,22 +96,23 @@ export default {
     box-sizing: border-box;
     padding: 10px 20px;
     margin: 5px 0;
-    border: 1px solid #283747;
+    border: 1px solid rgb(78, 0, 0);
+    border-radius: 10px;
 }
 .signUp_user button {
     width: 100%;
     height: 40px;
     color: #ffffff;
-    background-image: linear-gradient(180deg, #a26464, #000);
+    background-image: linear-gradient(180deg, #5c1c1c, #000);
     border: 1px solid #e5e7e9;
-    border-radius: 5px;
+    border-radius: 10px;
     padding: 10px 25px;
-    margin: 5px 0 25px 0;
+    margin: 10px 0 25px 0;
     font-weight: bold;
 }
 .signUp_user button:hover {
-    color: #e5e7e9;
-    background: rgb(109, 64, 73);
+    color: black;
+    background: linear-gradient(180deg, rgb(180, 180, 180),rgb(17, 17, 17));
     border: 1px solid #000000;
     cursor: pointer;
 }
